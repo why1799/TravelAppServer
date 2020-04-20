00d9bb7888132bc271364bce9b356bc96c3023b2
Артём Харченко <why1799@gmail.com>
2020-04-20 22:13:52 +0300

    Добавлена страница с поездками

---
+++ TravelAppServer/Pages/Trips.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelAppServer.Pages
+{
+    public class TripsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
